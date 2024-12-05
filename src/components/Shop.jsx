1110f55922e3f92635a9376be04ed0f90233f2fb
shawki walid <shawkighorab@gmail.com>
2024-12-05 10:41:04 +0200

    Solving Prop Drilling by component composition

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,10 @@
-export default function Shop({ onAddItemToCart }) {
+export default function Shop({children}) {
   return (
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
 
       <ul id="products">
-       
+       {children}
       </ul>
     </section>
   );
